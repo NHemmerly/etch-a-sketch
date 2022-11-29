47dde340bb585307e2f0ac5aaf70f2c714ba361a
NeilH <neilhemm0@gmail.com>
2022-11-28 21:35:25 -0500

    Create a 16x16 grid of uneven rectangles
    Code for the grid was found on stack overflow at:
    https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
    
    My own styling will be appended to the grid to meet the
    requirements of the project

---
+++ script.js
@@ -0,0 +1,15 @@
+//Create a variable 'grid' to store the container that will hold grid elements
+const grid = document.querySelector(".grid");
+
+//Function to create a grid. Will later create dynamically sized grids
+function createGrid (rows, columns) {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-columns', columns);
+    for (let c = 0; c < (rows * columns); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        grid.appendChild(cell).className = "grid-item";
+    };
+};
+
+createGrid(16, 16);
