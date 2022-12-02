cebacee297ac164b6f76c3b3c972ed38641dc1ed
NeilH <neilhemm0@gmail.com>
2022-12-01 21:33:46 -0500

    Add basic logic for adding pixels to grid

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 //Create a variable 'grid' to store the container that will hold grid elements
 const grid = document.querySelector(".grid");
 
+let mousedown = false;
+document.body.onmousedown = () => (mousedown = true);
+document.body.onmouseup = () => (mousedown = false);
+
 //Function to create a grid. Will later create dynamically sized grids
 function createGrid (rows, columns) {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-columns', columns);
     for (let c = 0; c < (rows * columns); c++) {
         let cell = document.createElement("div");
+        cell.addEventListener('mouseover', addPixel);
+        cell.addEventListener('mousedown', addPixel);
         grid.appendChild(cell).className = "grid-item";
     };
 };
+
+//When Mousebutton is down and hovering over the canvas, add a pixel of desired color
+function addPixel(e) {
+    if (e.type == 'mouseover' && !mousedown) {
+        return;
+    }
+    else if (e.type == 'mouseover' && mousedown) {
+        e.target.style.setProperty('background-color', 'black');
+    }
+}
+
+//User can select a different color, variables are returned to decide the color
 
 createGrid(16, 16);
